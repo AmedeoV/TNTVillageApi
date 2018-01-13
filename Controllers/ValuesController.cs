f66fcfb839abd4899491e5d874d846744d1cec81
avertullo86 <amedeo.vertullo@gmail.com>
2018-01-13 17:49:20 +0000

    creating the post and trying to scrape the response

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,44 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System;
+    using HtmlAgilityPack;
+    using Microsoft.AspNetCore.Mvc;
 
-namespace TNTVillageApi.Controllers
-{
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
-    {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+    namespace TNTVillageApi.Controllers {
+        [Route ("api/[controller]")]
+        public class ValuesController : Controller {
+            private static readonly HttpClient client = new HttpClient ();
+            // GET api/values
+            [HttpGet]
+            public IEnumerable<string> Get () {
+                return new string[] { "value1", "value2" };
+            }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+            // GET api/values/5
+            [HttpGet ("{searchTerm}")]
+            public async Task<string> GetAsync (string searchTerm) {
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+                var xxx = await DoPostAsync (searchTerm);
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+                HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument ();
+                htmlDocument.LoadHtml (xxx);
+                var tot_pag_addr = htmlDocument.DocumentNode.SelectSingleNode ("//div[@class='pagination']/form/span/b[3]/text()").InnerHtml;
+                var result_table_addr = htmlDocument.DocumentNode.SelectSingleNode ("//div[@class='showrelease_tb']/table/tr").InnerHtml;
+                var title_addr = htmlDocument.DocumentNode.SelectSingleNode ("./td[7]/a/text()");
+                var desc_addr = htmlDocument.DocumentNode.SelectSingleNode ("./td[7]/text()");
+                var leech_addr = htmlDocument.DocumentNode.SelectSingleNode ("./td[4]/text()");
+                var seed_addr = htmlDocument.DocumentNode.SelectSingleNode ("./td[5]/text()");
+                // var dl_addr = htmlDocument.DocumentNode.SelectSingleNode ("//div[@class='showrelease_tb']/table/tr[{}]/td[1]/a/@href");
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+                // tot_pag_addr = "//div[@class='pagination']/form/span/b[3]/text()"
+                // result_table_addr = "//div[@class='showrelease_tb']/table/tr"
+                // title_addr = "./td[7]/a/text()"
+                // desc_addr = "./td[7]/text()"
+                // leech_addr = "./td[4]/text()"
+                // seed_addr = "./td[5]/text()"
+                // dl_addr = "//div[@class='showrelease_tb']/table/tr[{}]/td[1]/a/@href"
+
+                return tot_pag_addr.ToString();
+            }
+
+            // POST api/values
+            [HttpPost]
+            public void Post ([FromBody] string value) { }
+
+            public async Task<string> DoPostAsync (string searchTerm) {
+
+                var values = new Dictionary<string, string> { { "srcrel", searchTerm },
+                        { "page", "1" },
+                        { "cat", "0" }
+                    };
+                var content = new FormUrlEncodedContent (values);
+
+                var response = await client.PostAsync ("http://www.tntvillage.scambioetico.org/src/releaselist.php", content);
+
+                var responseString = await response.Content.ReadAsStringAsync ();
+
+                return responseString;
+            }
+
+            // PUT api/values/5
+            [HttpPut ("{id}")]
+            public void Put (int id, [FromBody] string value) { }
+
+            // DELETE api/values/5
+            [HttpDelete ("{id}")]
+            public void Delete (int id) { }
         }
     }
-}
